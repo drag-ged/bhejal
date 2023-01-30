7ba195eebe97f5a674bab03bdb7657ac0ce22527
drag-ged <ghoshdebo2000@gmail.com>
2023-01-30 10:15:38 +0530

    updated peak finding algo

--- peak_finding.py
+++ peak_finding.py
@@ -7,26 +7,28 @@ import json
 import base64
 from io import BytesIO
 
-df = pd.read_csv('forward_peak.csv')
-data = df['I_current (Micro Amp)']
-data = np.array(data)
-peaks = find_peaks(data,threshold=3)
-local_maxima = argrelextrema(data, np.greater)
-# print(data[local_maxima[0]])
-# print(data[peaks[0]])
-plt.plot(data,label = 'I_current (Micro Amp)')
-plt.plot(local_maxima[0], data[local_maxima],'ro',label = 'local maxima')
-
-plt.plot(peaks[0],data[peaks[0]],'bo',label = 'peak')
-plt.xlabel ('Data Points')
+df = pd.read_csv('sample_data.csv')
+voltage = df['Step']
+current = df['I_current (Micro Amp)']
+voltage_data = np.array(voltage)
+current_data = np.array(current)
+peaks = find_peaks(current_data,distance=90,height=0)
+local_maxima = argrelextrema(current_data, np.greater)
+# print(current_data[local_maxima[0]])
+# print(current_data[peaks[0]])
+plt.plot(voltage,current,'g',label = 'voltammetric curve', )
+plt.plot(voltage[local_maxima[0]], current[local_maxima[0]],'ro',label = 'local maxima')
+plt.plot(voltage[peaks[0]],current[peaks[0]],'bo',label = 'peak')
+plt.grid()
+plt.xlabel ('Voltage')
 plt.ylabel ('I_current (Micro Amp)')
 plt.legend()
 
 print("local maximas : ")
-print(data[local_maxima[0]])
+print(current_data[local_maxima[0]])
 
 print("peaks : ")
-print(data[peaks[0]])
+print(current_data[peaks[0]])
 buf = BytesIO()
 plt.savefig(buf,format='png')
 buf.seek(0)
@@ -34,9 +36,7 @@ plt.savefig('hi.png')
 plt.show()
 image_based64 = base64.b64encode(buf.getvalue()).decode('utf-8')
 
-#print(image_based64)
-
-output_data = {'local_maxima' : local_maxima[0].tolist(), 'peaks' : peaks[0].tolist(),'image':image_based64}
+output_data = {'peaks' : current[peaks[0]].tolist(),'image':image_based64}
 print(output_data)
 with open("local_maxima.json","w") as f:
     json.dump(output_data,f)
